8589105cfb5ecf369eff9268f94b642d5fc41ed8
Philip S(D) <philipsteininger@gmail.com>
2018-09-04 19:39:36 -0700

    logo js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,13 @@
 $("document").ready(function () {
 
 
-    $(window).scroll(function () {
-        if (document.documentElement.scrollTop > document.documentElement.scrollHeight / 5) {
-            $("#nav-logo").css("display", "none");
-        } else {
-            $("#nav-logo").css("display", "inline-block");
-        }
+    // $(window).scroll(function () {
+    //     if (document.documentElement.scrollTop > document.documentElement.scrollHeight / 5) {
+    //         $("#nav-logo").css("display", "none");
+    //     } else {
+    //         $("#nav-logo").css("display", "inline-block");
+    //     }
 
-    });
+    // });
 
 });
