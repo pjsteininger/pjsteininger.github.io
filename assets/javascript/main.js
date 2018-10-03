d6feefabaa3229e8cba8a24f205516244b74be4d
Philip S(L) <philipsteininger@gmail.com>
2018-10-02 23:46:43 -0700

    changing icon code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,12 @@
 $("document").ready(function () {
 
+    // (function() {
+    //     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    //     link.type = 'image/x-icon';
+    //     link.rel = 'icon';
+    //     link.href = 'assets/images/icons/blocks8.png';
+    //     document.getElementsByTagName('head')[0].appendChild(link);
+    // })();
 
     // $(window).scroll(function () {
     //     if (document.documentElement.scrollTop > document.documentElement.scrollHeight / 5) {
